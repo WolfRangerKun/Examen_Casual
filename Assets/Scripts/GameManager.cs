3d8b547dc5cc5865b365e2d7c300014feade9908
WolfRangerKun <80328108+WolfRangerKun@users.noreply.github.com>
2021-06-07 18:33:36 -0400

    pornografiadeenanos

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    public bool gameRunning;
+    int tiempo = 30;
+    public TMP_Text txtTimer;
+
+    private void Start()
+    {
+        StartCoroutine(Tiempo());
+        gameRunning = true;
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        txtTimer.text = "Timer " + tiempo;
+        if (tiempo <= 0)
+        {
+            Debug.Log("Se acabo el Tiempo mano");
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ChangedGameRunningState();
+        }
+    }
+
+    IEnumerator Tiempo()
+    {
+        while (tiempo > 0)
+        {
+            yield return new WaitForSeconds(1);
+            tiempo--;
+        }
+    }
+
+    public void ChangedGameRunningState()
+    {
+        gameRunning = !gameRunning;
+
+        if (gameRunning)
+        {
+            Time.timeScale = 1f;
+
+            AudioSource[] audios = FindObjectsOfType<AudioSource>();
+            foreach (AudioSource a in audios)
+            {
+                a.Play();
+            }
+        }
+        else
+        {
+            Time.timeScale = 0f;
+
+            AudioSource[] audios = FindObjectsOfType<AudioSource>();
+            foreach (AudioSource a in audios)
+            {
+                a.Pause();
+            }
+        }
+    }
+
+    public bool IsGameRunning()
+    {
+        return gameRunning;
+    }
+}
