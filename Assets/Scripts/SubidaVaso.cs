1e7cdd6fb2ad700993d37a0da99beb72cbd19d19
WolfRangerKun <80328108+WolfRangerKun@users.noreply.github.com>
2021-06-12 20:08:56 -0400

    Ayuda

--- Assets/Scripts/SubidaVaso.cs
+++ Assets/Scripts/SubidaVaso.cs
@@ -2,41 +2,66 @@ using UnityEngine;
 
 public class SubidaVaso : MonoBehaviour
 {
-    public bool paArriba, paAsbajo, entradaObjetoAbajo, salidaObjetoAbajo;
+    public bool paArriba, paAsbajo, entradaObjetoAbajo, salidaObjetoAbajo, cosaPlayer;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Vaso") && paArriba)
+        if (paArriba)
         {
-            other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 2;
-            TransparenciaSegundoNivel.intanse.objectosArriba.Add(other.gameObject);
+            if (other.CompareTag("Vaso"))
+            {
+                other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 2;
+                TransparenciaSegundoNivel.intanse.objectosArriba.Add(other.gameObject);
+            }
+            if (other.CompareTag("Atomo"))
+            {
+                other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 2;
+                TransparenciaSegundoNivel.intanse.objectosArriba.Add(other.gameObject);
+            }
         }
 
-        if (other.CompareTag("Vaso") && paAsbajo)
+
+        if (paAsbajo)
         {
-            TransparenciaSegundoNivel.intanse.objectosArriba.Remove(other.gameObject);
-            other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 0;
+            if (other.CompareTag("Vaso"))
+            {
+                TransparenciaSegundoNivel.intanse.objectosArriba.Remove(other.gameObject);
+                other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 0;
+            }
+            if (other.CompareTag("Atomo"))
+            {
+                TransparenciaSegundoNivel.intanse.objectosArriba.Remove(other.gameObject);
+                other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 0;
+            }
         }
 
-        if (other.CompareTag("Atomo") && paArriba)
+        if (entradaObjetoAbajo)
         {
-            other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 2;
-            TransparenciaSegundoNivel.intanse.objectosArriba.Add(other.gameObject);
+            if ((other.CompareTag("Vaso") || other.CompareTag("Atomo")))
+            {
+                TransparenciaSegundoNivel.intanse.objectosAbajo.Add(other.gameObject);
+            }
         }
 
-        if (other.CompareTag("Atomo") && paAsbajo)
+        if (salidaObjetoAbajo)
         {
-            TransparenciaSegundoNivel.intanse.objectosArriba.Remove(other.gameObject);
-            other.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 0;
+            if ((other.CompareTag("Vaso") || other.CompareTag("Atomo")))
+            {
+                TransparenciaSegundoNivel.intanse.objectosAbajo.Remove(other.gameObject);
+            }
         }
 
-        //if((other.CompareTag("Vaso") || other.CompareTag("Atomo")) && entradaObjetoAbajo)
-        //{
-        //    TransparenciaSegundoNivel.intanse.objectosAbajo.Add(other.gameObject);
-        //}
-
-        //if ((other.CompareTag("Vaso") || other.CompareTag("Atomo")) && salidaObjetoAbajo)
-        //{
-        //    TransparenciaSegundoNivel.intanse.objectosAbajo.Remove(other.gameObject);
-        //}
+        if (other.CompareTag("Player") && cosaPlayer)
+        {
+            Debug.Log("porno");
+            foreach (GameObject g in TransparenciaSegundoNivel.intanse.objectosArriba)
+            {
+                g.SetActive(false);
+            }
+            foreach (GameObject g in TransparenciaSegundoNivel.intanse.objectosAbajo)
+            {
+                g.SetActive(true);
+            }
+        }
     }
+    
 }
--- Assets/Scripts/TriggerHide.cs
+++ Assets/Scripts/TriggerHide.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class TriggerHide : MonoBehaviour
 {
     public static TriggerHide intance;
     public bool isTriggerArriba;
+    public GameObject entrada, salida;
 
     private void Awake()
     {
@@ -19,6 +22,11 @@ public class TriggerHide : MonoBehaviour
                 CollisionEntrySecondFlor.intanse.triggerArriba.SetActive(false);
                 CollisionEntrySecondFlor.intanse.triggerAbajo.SetActive(true);
             }
+
+            if (!isTriggerArriba)
+            {
+                StartCoroutine(TiempoSetActivesExit());
+            }
         }
     }
 
@@ -28,6 +36,21 @@ public class TriggerHide : MonoBehaviour
         if (other.CompareTag("Player") && !isTriggerArriba)
         {
             TransparenciaSegundoNivel.intanse.modo = TransparenciaSegundoNivel.MODO.HIDE;
+            StartCoroutine(TiempoSetActivesEntry());
         }
+    }
+
+    IEnumerator TiempoSetActivesEntry()
+    {
+        entrada.SetActive(false);
+        yield return new WaitForSeconds(.2f);
+        salida.SetActive(true);
+    }
+    IEnumerator TiempoSetActivesExit()
+    {
+        entrada.SetActive(true);
+        yield return new WaitForSeconds(.2f);
+        salida.SetActive(false);
+
     }
 }
