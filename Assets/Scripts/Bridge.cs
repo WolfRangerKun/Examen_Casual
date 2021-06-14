9c5906104fe135367257f19d68546b64467df689
Gaboxz <80337191+Gaboxz@users.noreply.github.com>
2021-06-14 19:38:57 -0400

    xdxde tu mama

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -15,8 +15,11 @@ public class Bridge : MonoBehaviour
             collision.gameObject.layer = 3;
             collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
             if (bridge == 1) game.active1 = true;
-            if (bridge == 2 )game.active2 = true;
+            if (bridge == 2) game.active2 = true;
             if (bridge == 3) game.active3 = true;
+            if (bridge == 4) game.active4 = true;
+            if (bridge == 5) game.active5 = true;
+            if (bridge == 6) game.active6 = true;
             collision.gameObject.layer = 0;
             gameObject.SetActive(false);
         }
