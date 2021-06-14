76200af7fbb80591865ae6b2f9ec5a60d457556b
WolfRangerKun <80328108+WolfRangerKun@users.noreply.github.com>
2021-06-14 19:52:31 -0400

    PORNORNORNORNONOR

--- Assets/Scripts/Fuego.cs
+++ Assets/Scripts/Fuego.cs
@@ -4,11 +4,79 @@ using UnityEngine;
 
 public class Fuego : MonoBehaviour
 {
+    public PlayerMovement player;
+    public float pushDireccion = 1f;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+        
+    }
+    public enum DIRECCION
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+    public DIRECCION direccionCaida;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Dano");
+            direccionCaida = DIRECCION.DOWN;
+            EmpujarDano(DIRECCION.DOWN);
+        }
+    }
+
+   
+
+    void EmpujarDano(DIRECCION direccioncaida)
+    {
+        direccioncaida = direccionCaida;
+        switch (direccioncaida)
+        {
+            case DIRECCION.UP:
+                player.transform.position = new Vector2(player.transform.position.x, player.transform.position.y + pushDireccion);
+                player.targetPosition.y += pushDireccion;
+                break;
+            case DIRECCION.DOWN:
+                player.transform.position = new Vector2(player.transform.position.x, player.transform.position.y - pushDireccion);
+                player.targetPosition.y -= pushDireccion;
+                break;
+            case DIRECCION.RIGHT:
+                player.transform.position = new Vector2(player.transform.position.x + pushDireccion, player.transform.position.y);
+                player.targetPosition.x += pushDireccion;
+                break;
+            case DIRECCION.LEFT:
+                player.transform.position = new Vector2(player.transform.position.x - pushDireccion, player.transform.position.y);
+                player.targetPosition.x -= pushDireccion;
+                break;
+        }
+    }
+
+    public void MovementFall(Collider2D other, DIRECCION dire)
+    {
+        dire = direccionCaida;
+        switch (dire)
+        {
+            case DIRECCION.UP:
+                other.transform.position = new Vector2(other.transform.position.x, other.transform.position.y + pushDireccion);
+                player.targetPosition.y += pushDireccion;
+                break;
+            case DIRECCION.DOWN:
+                other.transform.position = new Vector2(other.transform.position.x, other.transform.position.y - pushDireccion);
+                player.targetPosition.y -= pushDireccion;
+                break;
+            case DIRECCION.RIGHT:
+                other.transform.position = new Vector2(other.transform.position.x + pushDireccion, other.transform.position.y);
+                player.targetPosition.x += pushDireccion;
+                break;
+            case DIRECCION.LEFT:
+                other.transform.position = new Vector2(other.transform.position.x - pushDireccion, other.transform.position.y);
+                player.targetPosition.x -= pushDireccion;
+                break;
         }
     }
 }
