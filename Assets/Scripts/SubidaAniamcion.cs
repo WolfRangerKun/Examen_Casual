435d2895c11ef0a5ac692cff85b9037cfc63894d
WolfRangerKun <80328108+WolfRangerKun@users.noreply.github.com>
2021-06-17 21:57:27 -0400

    Animacion del monito jajalol

--- Assets/Scripts/SubidaAniamcion.cs
+++ Assets/Scripts/SubidaAniamcion.cs
@@ -6,10 +6,12 @@ using DG.Tweening;
 public class SubidaAniamcion : MonoBehaviour
 {
     public float pushDireccion = 1f;
+    GameObject playerObj;
     Vector3 player;
     Vector3 upPosition;
     Vector3 leftPosition;
     Vector3 rightPosition;
+    Vector3 rotation;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -26,9 +28,11 @@ public class SubidaAniamcion : MonoBehaviour
     private void Update()
     {
         player = GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<PlayerMovement>().targetPosition;
+        playerObj = GameObject.FindGameObjectWithTag("Player");
         upPosition = player + new Vector3(0, 1, 0);
         leftPosition = player + new Vector3(-1f, 0, 0);
         rightPosition = player + new Vector3(1f, 0, 0);
+        rotation = playerObj.transform.position;
     }
 
     IEnumerator AnimacionSubida(Collider2D other)
@@ -41,10 +45,12 @@ public class SubidaAniamcion : MonoBehaviour
             other.gameObject.transform.DOMove(rightPosition, .1f);
             other.gameObject.GetComponent<PlayerMovement>().targetPosition += new Vector3(1f, 0, 0);
             yield return new WaitForSeconds(.5f);
+            other.gameObject.transform.DORotate((rotation + new Vector3(0, 0, -90)), 1);
             other.gameObject.transform.DOMove(upPosition, 1);
             other.gameObject.GetComponent<PlayerMovement>().targetPosition += new Vector3(0, 1, 0);
             yield return new WaitForSeconds(.5f);
-            other.gameObject.transform.DOMove(leftPosition, .3f);
+            other.gameObject.transform.DOMove(leftPosition, .5f);
+            other.gameObject.transform.DORotate((rotation + new Vector3(0, 0, 0)), 1f);
             other.gameObject.GetComponent<PlayerMovement>().targetPosition += new Vector3(-1f, 0, 0);
             yield return new WaitForSeconds(.1f);
             GetComponent<BoxCollider2D>().isTrigger = false;
