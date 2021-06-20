c5a3cc3b77ef590314b79bb49efa4db075b423f6
Gaboxz <80337191+Gaboxz@users.noreply.github.com>
2021-06-19 20:21:11 -0400

    bug de oxigeno uwu

--- Assets/Scripts/SegundoPiso.cs
+++ Assets/Scripts/SegundoPiso.cs
@@ -7,16 +7,17 @@ public class SegundoPiso : MonoBehaviour
     public PlayerMovement player;
     public bool stayPiso2;
     public List<GameObject> off;
-    public bool puente,  segundoPisoObjeto;
+    public bool puente;
+    public Objeto obj;
 
     public void Start()
     {
         player = FindObjectOfType<PlayerMovement>();
-
+        obj = FindObjectOfType<Objeto>();
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && collision.CompareTag("Objeto"))
+        if (collision.CompareTag("Player"))
         {
             //player.distanceRayCollision = 0.1f;
             off[0].layer = 0;
@@ -26,13 +27,16 @@ public class SegundoPiso : MonoBehaviour
             stayPiso2 = true;
             if (puente) off[4].layer = 0;
         }
-
-        
+        if (collision.gameObject.layer == 6 && collision.CompareTag("Atomo") || collision.CompareTag("Vaso") || collision.CompareTag("Bridges"))
+        {
+            obj.primerPiso = false;
+            obj.enabled = false;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && collision.CompareTag("Objeto"))
+        if (collision.CompareTag("Player"))
         {
             //player.distanceRayCollision = 1f;
             off[0].layer = 3;
@@ -41,6 +45,11 @@ public class SegundoPiso : MonoBehaviour
             off[3].layer = 3;
             stayPiso2 = false;
             if (puente) off[4].layer = 3;
+        }
+        if (collision.gameObject.layer == 6 && collision.CompareTag("Atomo") || collision.CompareTag("Vaso") || collision.CompareTag("Bridges"))
+        {
+            obj.primerPiso = true;
+            obj.enabled = true;
         }
 
 
