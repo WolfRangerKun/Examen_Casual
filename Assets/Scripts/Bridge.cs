3426ed74202fc78673ddab68ecb137de50e459cc
Gaboxz <80337191+Gaboxz@users.noreply.github.com>
2021-06-30 23:20:05 -0400

    tu mama

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -5,22 +5,20 @@ using UnityEngine;
 public class Bridge : MonoBehaviour
 {
     public BridgeManager game;
-    public int bridge;
-    
+    public static bool brig;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bridges"))
         {
+            collision.gameObject.GetComponent<Objeto>().contacts[0].SetActive(false);
+            collision.gameObject.GetComponent<Objeto>().contacts[1].SetActive(false);
+            collision.gameObject.GetComponent<Objeto>().contacts[2].SetActive(false);
+            collision.gameObject.GetComponent<Objeto>().contacts[3].SetActive(false);
+            collision.gameObject.GetComponent<Objeto>().enabled = false;
+            brig = true;
             collision.transform.position = gameObject.transform.position;
             collision.gameObject.layer = 3;
             collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
-            collision.gameObject.GetComponent<Objeto>().enabled = false;
-            if (bridge == 1) game.active1 = true;
-            if (bridge == 2) game.active2 = true;
-            if (bridge == 3) game.active3 = true;
-            if (bridge == 4) game.active4 = true;
-            if (bridge == 5) game.active5 = true;
-            if (bridge == 6) game.active6 = true;
             collision.gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder = 2;
             collision.gameObject.layer = 0;
             gameObject.SetActive(false);
