24c24fd036c5d6094529caa0d6aa26bfbec99497
Gaboxz <80337191+Gaboxz@users.noreply.github.com>
2021-07-01 22:51:45 -0400

    tu vieja la caliente 2

--- Assets/Scripts/TextoTutorial.cs
+++ Assets/Scripts/TextoTutorial.cs
@@ -6,12 +6,20 @@ public class TextoTutorial : MonoBehaviour
 {
     public List<string> dialogues;
     public bool derecha;
+    public int time;
     private DialogueSistem dialogeSistem;
     private void Start()
     {
         dialogeSistem = FindObjectOfType<DialogueSistem>();
     }
-    public void OnTriggerStay2D(Collider2D other)
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Time.timeScale = 1f;
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
@@ -19,21 +27,27 @@ public class TextoTutorial : MonoBehaviour
             {
                 dialogeSistem.panelDirection = DialogueSistem.PanelDirection.DERECHA;
                 DialogueSistem.instance.ShowDialogue(dialogues[0]);
+                StartCoroutine(TimingStop());
             }
             else
             {
                 dialogeSistem.panelDirection = DialogueSistem.PanelDirection.IZQUIERDA;
                 DialogueSistem.instance.ShowDialogue(dialogues[0]);
+                StartCoroutine(TimingStop());
             }
         }
     }
-
-
     public void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             DialogueSistem.instance.HideDialogue();
         }
+    }
+
+    IEnumerator TimingStop()
+    {
+        yield return new WaitForSeconds(time);
+        Time.timeScale = 0f;
     }
 }
