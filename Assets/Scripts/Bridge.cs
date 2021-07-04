07325ab2b4c6d7745f981d87f0a9197e08fb54f2
Gaboxz <80337191+Gaboxz@users.noreply.github.com>
2021-07-03 21:32:51 -0400

    xd

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Bridge : MonoBehaviour
 {
     public BridgeManager game;
-    public static bool brig;
     public bool lateral;
     public AudioSource sonidoCheck;
     private void OnCollisionEnter2D(Collision2D collision)
@@ -13,19 +12,21 @@ public class Bridge : MonoBehaviour
         if (collision.gameObject.CompareTag("Bridges"))
         {
             sonidoCheck.Play();
-            collision.gameObject.GetComponent<Objeto>().contacts[0].SetActive(false);
-            collision.gameObject.GetComponent<Objeto>().contacts[1].SetActive(false);
-            collision.gameObject.GetComponent<Objeto>().contacts[2].SetActive(false);
-            collision.gameObject.GetComponent<Objeto>().contacts[3].SetActive(false);
-            collision.gameObject.GetComponent<Objeto>().enabled = false;
-            brig = true;
+            if (lateral)
+            {
+                collision.gameObject.GetComponent<Objeto>().contacts[0].SetActive(false);
+                collision.gameObject.GetComponent<Objeto>().contacts[1].SetActive(false);
+                collision.gameObject.GetComponent<Objeto>().contacts[4].SetActive(true);
+            }
+            else
+            {
+                collision.gameObject.GetComponent<Objeto>().contacts[2].SetActive(false);
+                collision.gameObject.GetComponent<Objeto>().contacts[3].SetActive(false);
+            }
+            //collision.gameObject.GetComponent<Objeto>().enabled = false;
             collision.transform.position = gameObject.transform.position;
             collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
             collision.gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder = 2;
-            if (lateral)
-            {
-                collision.gameObject.GetComponent<Objeto>().contacts[4].SetActive(false);
-            }
             collision.gameObject.layer = 3;
             collision.gameObject.layer = 0;
             gameObject.SetActive(false);
