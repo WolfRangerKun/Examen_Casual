97b20bfed931772a7a8d4a8a5bd75ec42d42d61c
Gaboxz <80337191+Gaboxz@users.noreply.github.com>
2021-07-05 16:39:16 -0400

    tu mama

--- Assets/BridgeTutorial.cs
+++ Assets/BridgeTutorial.cs
@@ -7,8 +7,24 @@ public class BridgeTutorial : MonoBehaviour
     public float time;
     public SegundoPiso segundoPiso;
     public GameObject limitBridges;
-    // Update is called once per frame
-    void Update()
+    public bool dspsDeSubir;
+    public IEnumerator Tming()
+    {
+        yield return new WaitForSeconds(0.5f);
+        limitBridges.SetActive(false);
+        yield return new WaitForSeconds(time);
+        dspsDeSubir = true;
+    }
+
+    private void Update()
+    {
+        if (dspsDeSubir)
+        {
+            OffOrOn();
+        }
+        
+    }
+    public void OffOrOn()
     {
         if (segundoPiso.libro[1].activeSelf == true)
         {
@@ -17,16 +33,7 @@ public class BridgeTutorial : MonoBehaviour
         if (segundoPiso.libro[1].activeSelf == false)
         {
             limitBridges.SetActive(true);
+            dspsDeSubir = false;
         }
-
-    }
-
-
-    public IEnumerator Tming()
-    {
-        yield return new WaitForSeconds(0.5f);
-        gameObject.SetActive(false);
-        yield return new WaitForSeconds(time);
-        //gameObject.SetActive(true);
     }
 }
